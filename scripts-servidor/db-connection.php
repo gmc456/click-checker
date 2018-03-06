f465e63c600c29f392d9570380086ef50e6b96c8
Francis Galvez <mgl645@inlumine.ual.es>
2018-03-06 15:16:48 +0100

    Modificado archivo conexion

--- scripts-servidor/db-connection.php
+++ scripts-servidor/db-connection.php
@@ -1,13 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$host = "localhost";
+$user = "root";
+$password = "root";
+$database = "myDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$mysqli = new mysqli($host, $user, $password, $database);
+
 // Check connection
-if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
+if($mysqli->connect_errno){
+  echo "Fallo al conectar a MySQL: (" . $mysqli -> connect_errno . ")" .
+  $mysqli->connect_error . "<br/>";
 }
+echo "Conectado " . $mysqli->host_info . "<br/>";
 ?>
